9c8c2c0e4f2a9e243a42ef85fddbf19156968986
Pete Zimbelman <pzimbelman@gmail.com>
2012-07-19 11:42:25 -0400

    some work on feed entries

--- lib/twithub/feed_entry.rb
+++ lib/twithub/feed_entry.rb
@@ -1,8 +1,20 @@
 module Twithub
   class FeedEntry
-    attr_reader :content
-    def initialize(content)
+    TWITTER = "twitter"
+    GITHUB = "github"
+    attr_reader :content, :posted_at, :origin
+    def initialize(content, posted_at, origin)
       @content = content
+      @posted_at = posted_at
+      @origin = origin
+    end
+
+    def self.from_twitter(content, posted_at)
+      new(content, posted_at, TWITTER)
+    end
+
+    def self.from_github(content, posted_at)
+      new(content, posted_at, GITHUB)
     end
   end
 end
--- spec/twithub/feed_entry_spec.rb
+++ spec/twithub/feed_entry_spec.rb
@@ -1,6 +1,30 @@
 require_relative '../../lib/twithub'
 describe Twithub::FeedEntry do
-  it "should have text content" do
-    Twithub::FeedEntry.new("my post").content.should == "my post"
+  let(:time) { Time.now }
+  describe "a basic entry with content and a time and a nonsense service" do
+    it "should give access to its attribtues" do
+      entry = Twithub::FeedEntry.new("my post", time, "foo")
+      entry.content.should == "my post"
+      entry.posted_at.should == time
+      entry.origin.should == "foo"
+    end
+  end
+
+  describe "creating a twitter feed entry" do
+    it "should create a valid entry with an origin of twitter" do
+      entry = Twithub::FeedEntry.from_twitter("the content", time)
+      entry.content.should == "the content"
+      entry.posted_at.should == time
+      entry.origin.should == Twithub::FeedEntry::TWITTER
+    end
+  end
+
+  describe "creating a github feed entry" do
+    it "should create a valid entry with an origin of github" do
+      entry = Twithub::FeedEntry.from_github("the content", time)
+      entry.content.should == "the content"
+      entry.posted_at.should == time
+      entry.origin.should == Twithub::FeedEntry::GITHUB
+    end
   end
 end
