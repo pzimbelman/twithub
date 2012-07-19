0b3b1f064a0f1b0a800308d286a26bd07a81c72a
Pete Zimbelman <pzimbelman@gmail.com>
2012-07-19 12:53:55 -0400

    remove behavior off of base entry class

--- lib/twithub/feed_entry.rb
+++ lib/twithub/feed_entry.rb
@@ -3,11 +3,5 @@ module Twithub
     TWITTER = "twitter"
     GITHUB = "github"
     attr_reader :content, :posted_at, :origin, :username
-    def initialize(params)
-      @content = params[:content]
-      @posted_at = params[:posted_at]
-      @username = params[:username]
-      @origin = params[:origin]
-    end
   end
 end
--- spec/twithub/feed_entry_spec.rb
+++ spec/twithub/feed_entry_spec.rb
@@ -1,15 +1,12 @@
 require_relative '../../lib/twithub'
 describe Twithub::FeedEntry do
-  let(:time) { Time.now }
-  let(:params) { {:content => "my post", :username => "pzimbelman", :posted_at => time,
-                  :origin => "foo" } }
-  describe "a basic entry with content and a time and a nonsense service" do
-    it "should give access to its attribtues" do
-      entry = Twithub::FeedEntry.new(params)
-      entry.content.should == "my post"
-      entry.username.should == "pzimbelman"
-      entry.posted_at.should == time
-      entry.origin.should == "foo"
+  describe "an empty base entry" do
+    it "should have no attributes filled out by default" do
+      entry = Twithub::FeedEntry.new
+      entry.content.should be_nil
+      entry.username.should be_nil
+      entry.posted_at.should be_nil
+      entry.origin.should be_nil
     end
   end
 end
